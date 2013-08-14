8e12ace3684ff6dee74394f12541348107bc1855
Aleksandr Filatov <greybax@gmail.com>
2013-08-14 11:36:25 -0700

    fix program v1.1

--- YandexDynDNS/Program.cs
+++ YandexDynDNS/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Xml;
@@ -11,21 +13,8 @@ namespace YandexDynDNS
         public static void Main(string[] args)
         {
             var currentIp = GetCurrentIp();
-            var domain = GetDomainFromFile();
-            var token = GetTokenFromFile();
 
-            var recordId = GetDomainRecord(
-                            "https://pddimp.yandex.ru/nsapi/get_domain_records.xml?" +
-                            "token=" + token +
-                            "&domain=" + domain);
-
-            EditARecord("https://pddimp.yandex.ru/nsapi/edit_a_record.xml?" +
-                           "token=" + token +
-                           "&domain=" + domain +
-                           //"&subdomain=www." + domain +
-                           "&record_id=" + recordId +
-                           //"&[ttl=<время жизни записи>]" +
-                           "&content=" + currentIp);
+            ChangeIpForDomainInfoCsv(currentIp);
         }
 
         private static string GetCurrentIp()
@@ -40,24 +29,85 @@ namespace YandexDynDNS
             return myCurrentIp;
         }
 
-        private static string GetDomainFromFile()
+        private static void ChangeIpForDomainInfoCsv(string myIp)
         {
-            return GetValueFromFile("domain.txt");
-        }
+            string[] allLines = File.ReadAllLines("domainInfo.csv");
 
-        private static string GetTokenFromFile()
-        {
-            return GetValueFromFile("token.txt");
-        }
-
-        private static string GetValueFromFile(string fileName)
-        {
-            using (var reader = new StreamReader(fileName))
+            foreach (var line in allLines)
             {
-                var value = reader.ReadToEnd();
+                var domainList = ParseCsvRow(line);
 
-                return value;
+                var recordId = GetDomainRecord(
+                "https://pddimp.yandex.ru/nsapi/get_domain_records.xml?" +
+                "token=" + domainList[0] +
+                "&domain=" + domainList[2]);
+
+                EditARecord("https://pddimp.yandex.ru/nsapi/edit_a_record.xml?" +
+                               "token=" + domainList[0] +
+                               "&domain=" + domainList[2] +
+                               (string.IsNullOrWhiteSpace(domainList[1]) ? string.Empty : "&subdomain=" + domainList[1]) +
+                               "&record_id=" + recordId +
+                               //"&[ttl=<время жизни записи>]" +
+                               "&content=" + myIp);
             }
+        }
+
+        public static string[] ParseCsvRow(string r)
+        {
+            string[] c;
+            var resp = new List<string>();
+            bool cont = false;
+            string cs = "";
+
+            c = r.Split(new[] { ',' }, StringSplitOptions.None);
+
+            foreach (string y in c)
+            {
+                string x = y;
+
+                if (cont)
+                {
+                    // End of field
+                    if (x.EndsWith("\""))
+                    {
+                        cs += "," + x.Substring(0, x.Length - 1);
+                        resp.Add(cs);
+                        cs = "";
+                        cont = false;
+                        continue;
+                    }
+                    // Field still not ended
+                    cs += "," + x;
+                    continue;
+                }
+
+                // Fully encapsulated with no comma within
+                if (x.StartsWith("\"") && x.EndsWith("\""))
+                {
+                    if ((x.EndsWith("\"\"") && !x.EndsWith("\"\"\"")) && x != "\"\"")
+                    {
+                        cont = true;
+                        cs = x;
+                        continue;
+                    }
+
+                    resp.Add(x.Substring(1, x.Length - 2));
+                    continue;
+                }
+
+                // Start of encapsulation but comma has split it into at least next field
+                if (x.StartsWith("\"") && !x.EndsWith("\""))
+                {
+                    cont = true;
+                    cs += x.Substring(1);
+                    continue;
+                }
+
+                // Non encapsulated complete field
+                resp.Add(x);
+            }
+
+            return resp.ToArray();
         }
 
         private static string GetDomainRecord(string uri)
@@ -97,5 +147,4 @@ namespace YandexDynDNS
             return isOk.Equals("ok");
         }
     }
-
 }
