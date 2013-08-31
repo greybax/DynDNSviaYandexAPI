587caed1b63b3d9c683e0956351f3f9f92ffde60
Aleksandr Filatov <greybax@gmail.com>
2013-08-31 13:10:22 -0700

    add log4net

--- YandexDynDNS/Properties/AssemblyInfo.cs
+++ YandexDynDNS/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -19,6 +18,8 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6f80cd1e-5bb3-4b68-b8a0-fc92b0058fb1")]
 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
